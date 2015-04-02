0ba866e511bfdd62224b8826210bd2e8bc121c8b
yonchul <yonch@softserveinc.com>
2015-04-02 18:32:07 +0300

    add npm and bower. add faundation

---
+++ faundation.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--    <link rel="icon" href="../../favicon.ico">-->
+
+    <title>Faundation Tamplate</title>
+
+<!--    <link rel="stylesheet" href="./bower_components/foundation/css/normalize.css"/>-->
+    <link rel="stylesheet" href="./bower_components/foundation/css/foundation.css"/>
+</head>
+<body>
+<div class="row">
+    <div class="large-3 columns">
+        <h1><img src="http://placehold.it/400x100&text=Logo"/></h1>
+    </div>
+    <div class="large-9 columns">
+        <ul class="right button-group">
+            <li><a href="#" class="button">Link 1</a></li>
+            <li><a href="#" class="button">Link 2</a></li>
+            <li><a href="#" class="button">Link 3</a></li>
+            <li><a href="#" class="button">Link 4</a></li>
+        </ul>
+    </div>
+</div>
+
+
+
+
+
+<div class="row">
+    <div class="large-12 columns">
+        <div class="orbit-container"><ul class="example-orbit orbit-slides-container" data-orbit="" style="height: 388px;">
+                <li class="active"><img src="http://placehold.it/1000x400&amp;text=[%20img%201%20]"></li>
+                <li><img src="http://placehold.it/1000x400&amp;text=[%20img%202%20]"></li>
+                <li><img src="http://placehold.it/1000x400&amp;text=[%20img%203%20]"></li>
+            </ul><a href="#" class="orbit-prev"><span></span></a><a href="#" class="orbit-next"><span></span></a><div class="orbit-timer paused"><span></span><div class="orbit-progress"></div></div><div class="orbit-slide-number"><span></span> of <span></span></div><div class="orbit-bullets-container"><ol class="orbit-bullets"><li data-orbit-slide="0"></li><li data-orbit-slide="1"></li><li data-orbit-slide="2"></li></ol></div></div>
+    </div>
+</div>
+
+
+
+<div class="row">
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>This is a content section.</h4>
+        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>This is a content section.</h4>
+        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>This is a content section.</h4>
+        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+
+</div>
+
+
+<div class="row">
+    <div class="large-12 columns">
+
+        <div class="panel">
+            <h4>Get in touch!</h4>
+
+            <div class="row">
+                <div class="large-9 columns">
+                    <p>We'd love to hear from you, you attractive person you.</p>
+                </div>
+                <div class="large-3 columns">
+                    <a href="#" class="radius button right">Contact Us</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+<footer class="row">
+    <div class="large-12 columns">
+        <hr/>
+        <div class="row">
+            <div class="large-6 columns">
+                <p>© Copyright no one at all. Go to town.</p>
+            </div>
+            <div class="large-6 columns">
+                <ul class="inline-list right">
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="./bower_components/jquery/src/jquery.js"></script>
+<script src="./bower_components/foundation/js/foundation.min.js"></script>
+</body>
+</html>
