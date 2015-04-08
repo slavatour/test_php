fb33e9d84fa50a5a2b50e8c3b8b2fb8ae4b3ad97
yonchul <yonch@softserveinc.com>
2015-04-08 14:51:51 +0300

    remove 'bower_components' folder with libs. remove zurb foundation. set bootstrap. add bootswatch. build first swig page

--- index.php
+++ index.php
@@ -1,35 +1,10 @@
 <?php
-    define('MAIN_PATH', 'test_php');
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="./public/libs/zurb-foundation/css/normalize.css"/>
-    <link rel="stylesheet" href="./public/libs/zurb-foundation/css/foundation.min.css"/>
-    <link rel="stylesheet" href="./public/css/style.css"/>
-    <link rel="stylesheet" href="./public/css/common.css"/>
-</head>
-<body>
 
-<!--  header  -->
-    <?php include('public/templates/header.php'); ?>
+require_once 'vendor/autoload.php';
 
-<!--  content  -->
-    <?php include('public/templates/content.php'); ?>
+$loader = new Twig_Loader_Filesystem('public/templates');
+$twig = new Twig_Environment($loader);
 
-<!--  social  -->
-    <?php include('public/templates/social.php'); ?>
+$template = $twig->loadTemplate('index.html');
 
-<!--  footer  -->
-    <?php include('public/templates/footer.php'); ?>
-
-    <script src="./bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="./bower_components/foundation/js/vendor/modernizr.js"></script>
-    <script src="./public/libs/zurb-foundation/js/foundation.min.js"></script>
-    <script>
-        $(document).foundation();
-    </script>
-</body>
-</html>
+echo $template->render(array());
