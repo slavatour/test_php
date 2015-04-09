8561eae5c8110f4ce566699855b406f29ab0728b
yonchul <yonch@softserveinc.com>
2015-04-09 18:59:04 +0300

    add modal

--- includes/db.class.php
+++ includes/db.class.php
@@ -1,10 +1,8 @@
 <?php
 
-namespace app;
-
-
 class db {
     private $_db;
+    private static $mysqli = null;
 
     const HOST = 'localhost';
     const USER = 'test-user';
@@ -12,13 +10,22 @@ class db {
     const BASE_NAME = 'test_db';
 
     private function __construct () {
-        $ob_mysqli = new \mysqli(self::HOST, self::USER, self::PASSWORD, self::BASE_NAME);
+        $ob_mysqli = new mysqli(self::HOST, self::USER, self::PASSWORD, self::BASE_NAME);
         if(!$ob_mysqli->connect_error) {
             $this->_db = $ob_mysqli;
+            $this->_db->query("SET NAMES 'utf8'");
             return $this->_db;
         }
         else {
             exit('No connect to server!');
         }
+    }
+
+    public function getConnection() {
+        if(self::$mysqli == null) {
+            $db = new db();
+            self::$mysqli = $db->_db;
+        }
+        return self::$mysqli;
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,62 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'includes/db.class.php';
+
+$data = array(
+    array(
+        'id' => '1',
+        'photo'=> 'public/images/sl1.jpg',
+        'name'=> 'name',
+        'description'=> 'desc',
+        'category'=> 'bick',
+        'price'=> '1300'
+    ),
+    array(
+        'id' => '1',
+        'photo'=> 'public/images/sl2.jpg',
+        'name'=> 'name',
+        'description'=> 'desc',
+        'category'=> 'bick',
+        'price'=> '1300'
+    ),
+    array(
+        'id' => '1',
+        'photo'=> 'public/images/sl3.jpg',
+        'name'=> 'name',
+        'description'=> 'desc',
+        'category'=> 'bick',
+        'price'=> '1300'
+    ),
+    array(
+        'id' => '1',
+        'photo'=> 'public/images/sl4.jpg',
+        'name'=> 'name',
+        'description'=> 'desc',
+        'category'=> 'bick',
+        'price'=> '1300'
+    ),
+    array(
+        'id' => '1',
+        'photo'=> 'public/images/sl5.jpg',
+        'name'=> 'name',
+        'description'=> 'desc',
+        'category'=> 'bick',
+        'price'=> '1300'
+    ),
+    array(
+        'id' => '1',
+        'photo'=> 'public/images/sl6.jpg',
+        'name'=> 'name',
+        'description'=> 'desc',
+        'category'=> 'bick',
+        'price'=> '1300'
+    )
+);
 
 $loader = new Twig_Loader_Filesystem('public/templates');
 $twig = new Twig_Environment($loader);
 
 $template = $twig->loadTemplate('index.html');
 
-echo $template->render(array());
+echo $template->render(array('products'=> $data));
